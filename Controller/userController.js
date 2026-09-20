39093f3f7a4ce250f917c6e09d4bb798d2dbd463
Sahil singh <sahilsingh.afl@gamil.com>
2026-09-20 09:40:40 +0530

    Complete

--- Controller/userController.js
+++ Controller/userController.js
@@ -98,8 +98,8 @@ export const logout = (req, res) => {
 
     res.clearCookie("token", {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
+        secure: false,
+        sameSite: "lax",
     });
 
     res.status(200).json({
